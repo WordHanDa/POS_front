2c99a3a26b5d3b3dc806c8393f5aa5336eca3141
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2026-09-12 17:37:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Special from './components/Special';
 import SpiritDetail from './components/SpiritDetail';
 import CartDrawer from './components/CartDrawer';
 
-const BASE_API = 'http://127.0.0.1:3002';
+const BASE_API = 'https://posserver-sigma.vercel.app';
 
 function App() {
   useEffect(() => {
